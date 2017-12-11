9af4633d2eef2d57eca96d4904d1448386176b42
ebrand0007@gmail.com <rosbuilder@centos7-dev2.area52.com>
2017-12-11 12:50:56 +0000

    vid 18

--- src/video_stream.cpp
+++ src/video_stream.cpp
@@ -208,7 +208,11 @@ int main(int argc, char** argv)
       //TOOD: delete ROS_INFO_STREAM("Raw video format: " << cap.get(CV_CAP_PROP_FORMAT) );
 
       //TODO: CV_CAP_PROP_MODE is specific to kinect to being set to CV_CAP_MODE_YUYV
-      cap.set(CV_CAP_PROP_MODE, CV_CAP_MODE_YUYV); //https://stackoverflow.com/questions/27496698/opencv-capture-yuyv-from-camera-without-rgb-conversion 
+      //cap.set(CV_CAP_PROP_MODE, CV_CAP_MODE_YUYV); 
+      cap.set(CV_CAP_PROP_MODE, CAP_MODE_GRAY);
+          //https://stackoverflow.com/questions/27496698/opencv-capture-yuyv-from-camera-without-rgb-conversion 
+          //https://docs.opencv.org/3.3.0/d4/d15/group__videoio__flags__base.html#gad0f42b32af0d89d2cee80dae0ea62b3d
+          //https://docs.opencv.org/3.3.0/d4/d15/group__videoio__flags__base.html
 
     }
     //catch (cv_bridge::Exception &e)
